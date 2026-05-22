8db6288b3ba88fa58373a8c5ab49b0fe297714b3
ArefOrumiehei <areforumiehei@gmail.com>
2026-05-22 12:08:45 +0330

    🌼 Create app routing using react-router

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import SentimentLanding from '../pages/landings/SentimentLanding';
 import TextAnalyzer from '../pages/text-analyzer/TextAnalyzer';
+import ProductSearch from '../pages/product-search/ProductSearch';
+import ProductDetails from '../pages/product-details/ProductDetails';
 
 export function AppRouter() {
   return (
@@ -8,6 +10,10 @@ export function AppRouter() {
       <Routes>
         <Route index element={<SentimentLanding />} />
         <Route path="/text-analyzer" element={<TextAnalyzer />} />
+        <Route path="/products" element={<Navigate to="/products/search" replace />} />
+        <Route path="/products/search" element={<ProductSearch />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
